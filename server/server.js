32733674082724fa347b10d23b4f01c9f8c7ea0e
User <lexin_wiesner@yahoo.com>
2023-09-21 21:25:20 -0700

    Added mongoose and stater code in modules

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,8 +8,10 @@ app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes.js')(app);
+//require('./routes/htmlRoutes.js')(app);
 
-app.listen(PORT, function () {
-  console.log(`Now listening on port: ${PORT}`);
+db.once('open', () => {
+  app.listen(PORT, function () {
+    console.log(`Now listening on port: ${PORT}`);
+  });
 });
