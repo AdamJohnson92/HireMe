09acf083ac00aa03002a982573a26d5bf404a3e9
Adam <adamgjohnson92@gmail.com>
2023-09-21 20:32:10 -0700

    Re-worked package.json to launch concurrently. set server port in server.js to 3001

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
+import Login from "./Components/Login"
+import { LoginContext } from './Context/LoginContext'
 
 function App() {
+
+  const [candidateLoggedIn, setCandidateLoggedIn] = useState(false)
+  const [userEmail, setUserEmail] = useState('')
+
   return (
     <div className="hire-app">
+      <LoginContext.Provider value={{userEmail, setUserEmail, candidateLoggedIn, setCandidateLoggedIn }}>
+        <Login></Login>
+      </LoginContext.Provider>
+
+      <h1>HELLO!</h1>
       
     </div>
   );
