c2770d676bc1573d2e23b6734a1ecd1b3d41c40f
User <lexin_wiesner@yahoo.com>
2023-09-30 12:45:49 -0700

    TypeDefs and Resolvers are finished

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -63,22 +63,63 @@ const resolvers = {
       return { token, user };
     },
     //Adding in update mutations on City, State, Education, and Skills
-    updateUserCity: async (parent, { userId, newCity }, context) => {
+    updateUserCity: async (parent, { email, newCity }, context) => {
       // if (!context.user) {
       //   throw new AuthenticationError('You need to be logged in!');
       // }
-      const updatedUser = await User.findByIdAndUpdate(userId, { userCity: newCity }, { new: true });
+      const updatedUser = await User.findOneAndUpdate({email}, { userCity: newCity }, { new: true });
       return updatedUser;
     },
-    updateUserState: async (parent, { userId, newState }, context) => {
+    updateUserState: async (parent, { email, newState }, context) => {
       // if (!context.user) {
       //   throw new AuthenticationError('You need to be logged in!');
       // }
-      const updatedUser = await User.findByIdAndUpdate(userId, { userState: newState }, { new: true });
+      const updatedUser = await User.findOneAndUpdate({email}, { userState: newState }, { new: true });
       return updatedUser;
     },
+    updateEdu: async (parent, { email, newEdu }, context) => {
+      // if (!context.user) {
+      //   throw new AuthenticationError('You need to be logged in!');
+      // }
+      const updatedUser = await User.findOneAndUpdate({email}, { education: newEdu }, { new: true });
+      return updatedUser;
+    },
+    addSkills: async (parent, { email, newSkills }, context) => {
+      // if (!context.user) {
+      //   throw new AuthenticationError('You need to be logged in!');
+      // }
+      const existingSkills = await Skills.find({ name: { $in: newSkills } });
+      const existingSkillNames = existingSkills.map(skill => skill.name);
+      const skillsToBeAdded = newSkills.filter(skill => !existingSkillNames.includes(skill));
 
+      // Add new skills to the Skills collection
+      const newAddedSkills = await Skills.insertMany(skillsToBeAdded.map(name => ({ name })));
 
+      const allSkillIds = [...existingSkills, ...newAddedSkills].map(skill => skill._id);
+
+      // Update the user's skills array
+      const updatedUser = await User.findOneAndUpdate(
+        { email },
+        { $addToSet: { skills: { $each: allSkillIds } } },
+        { new: true }
+      ).populate('skills');
+
+      return updatedUser;
+    },
+    removeSkills: async (parent, { email, oldSkills }, context) => {
+      // Find the skill IDs from the Skills collection that match the names provided in oldSkills.
+      const skillsToRemove = await Skills.find({ name: { $in: oldSkills } });
+      const skillIdsToRemove = skillsToRemove.map(skill => skill._id);
+
+      // Update the user's skills array by pulling these skill IDs.
+      const updatedUser = await User.findOneAndUpdate(
+        { email },
+        { $pull: { skills: { $in: skillIdsToRemove } } }, // using $in operator to match any values in the provided array
+        { new: true }
+      ).populate('skills');
+
+      return updatedUser;
+    },
 
   },
 };
